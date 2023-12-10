f8cc0c53e3987ef0a264b287ec1a1c4346e5c691
MOONJJJJJ <yangmoonjj5@gmail.com>
2023-12-11 01:41:41 +0900

    완료

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ import Detail from "./routes/Detail";
 function App() {
     return (<Router>
         <Routes>
-            <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
-            <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+            <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />} />
+            <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
         </Routes>
     </Router>);
 }
